2bf4c310dded4dab204ecc0adad48dc57a97ba6d
libreninja <sjbenner@gmail.com>
2015-08-03 21:56:23 -0700

    symlink vimrc on install

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ if [ ! -d "$HOME/.dotfiles" ]; then
     git clone --depth=1 https://github.com/libreninja/dotfiles.git "$HOME/.dotfiles"
     cd "$HOME/.dotfiles"
     [ "$1" = "ask" ] && export ASK="true"
+    ln -sf "$HOME/.dotfiles/vim" "$HOME/.vim"
     ln -sf "$HOME/.dotfiles/vim/vimrc" "$HOME/.vimrc"
 else
     echo "dotfiles already installed"
