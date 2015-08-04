4e9c0498ed2bb685eed6246489935f966a2f1c18
libreninja <sjbenner@gmail.com>
2015-08-03 21:48:57 -0700

    added install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ ! -d "$HOME/.dotfiles" ]; then
+    echo "fresh dotfile installation"
+    git clone --depth=1 https://github.com/libreninja/dotfiles.git "$HOME/.dotfiles"
+    cd "$HOME/.dotfiles"
+    [ "$1" = "ask" ] && export ASK="true"
+    ln -sf "$HOME/.dotfiles/vim/vimrc" "$HOME/.vimrc"
+else
+    echo "dotfiles already installed"
+fi
